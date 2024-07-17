3a3ee8a1fb685e814096ba4887d9d17a229699ce
Artsiom <masloartem20@gmail.com>
2024-07-17 22:19:27 +0300

    Add TimePicker and Medals for prize-places

--- src/UI/judgment/time-matches/result-table-time-matches.jsx
+++ src/UI/judgment/time-matches/result-table-time-matches.jsx
@@ -1,58 +1,76 @@
 /* eslint-disable prettier/prettier */
-import { Table, Tooltip, Flex } from 'antd'
+import GoldMedal from '@src/assets/img/gold-medal.png';
+import SilverMedal from '@src/assets/img/silver-medal.png';
+import BronzeMedal from '@src/assets/img/bronze-medal.png';
+import { Table, Tooltip, Flex } from 'antd';
 
-function ResultTableTimeMatches(){
-    const columns = [
-        {
-            title: <Tooltip title="Место"></Tooltip>,
-            dataIndex: 'medal',
-            key: 'medal'
-        },
-        {
-            title: <Tooltip title="Место">Место</Tooltip>,
-            dataIndex: 'place',
-            key: 'place'
-        },
-        {
-            title: <Tooltip title="Участник">Участники</Tooltip>,
-            dataIndex: 'participants',
-            key: 'participants'
-        },
-        {
-            title: <Tooltip title="Лучшее время">Лучшее время</Tooltip>,
-            dataIndex: 'best_attempt',
-            key: 'best_attempt'
-        },
-        
-    ]
+function ResultTableTimeMatches() {
+  const columns = [
+    {
+      title: <Tooltip></Tooltip>,
+      key: 'medal',
+      render: (record) => (
+        <div>
+          <img
+            src={
+              record.place === '1'
+                ? GoldMedal
+                : record.place === '2'
+                ? SilverMedal
+                : record.place === '3'
+                ? BronzeMedal
+                : ''
+            }
+          style={{ width: '50px',height: '50px'}}/>
+        </div>
+      ),
+    },
+    {
+      title: <Tooltip title="Место">Место</Tooltip>,
+      dataIndex: 'place',
+      key: 'place',
+    },
+    {
+      title: <Tooltip title="Участник">Участники</Tooltip>,
+      dataIndex: 'participants',
+      key: 'participants',
+    },
+    {
+      title: <Tooltip title="Лучшее время">Лучшее время</Tooltip>,
+      dataIndex: 'best_attempt',
+      key: 'best_attempt',
+    },
+  ];
 
-    const data = [
-        {
-            place: '1',
-            participants: 'Иванов Иван Иванович',
-            best_attempt: '2:49:35'
-        },
-        {
-            place: '2',
-            participants: 'Егоров Егор Егорович',
-            best_attempt: '2:41:30'
-        },
-        {
-            place: '3',
-            participants: 'Кириллов Кирилл Кириллович',
-            best_attempt: '2:40:12'
-        }
-    ]
+  const data = [
+    {
+      place: '1',
+      participants: 'Иванов Иван Иванович',
+      best_attempt: '2:49:35',
+      medal: GoldMedal,
+    },
+    {
+      place: '2',
+      participants: 'Егоров Егор Егорович',
+      best_attempt: '2:41:30',
+      medal: SilverMedal,
+    },
+    {
+      place: '3',
+      participants: 'Кириллов Кирилл Кириллович',
+      best_attempt: '2:40:12',
+      medal: BronzeMedal,
+    },
+  ];
 
-    return (
-        <Flex vertical gap="large">
-            <Table
-            pagination={false}
-            columns={columns}
-            dataSource={data}
-            />
-        </Flex>
-    )
+  return (
+    <Flex vertical gap="large">
+      <Table
+        pagination={false}
+        columns={columns}
+        dataSource={data}
+      />
+    </Flex>
+  );
 }
-
 export default ResultTableTimeMatches
--- src/UI/judgment/time-matches/table-time-matches.jsx
+++ src/UI/judgment/time-matches/table-time-matches.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Table, Tooltip, Flex } from 'antd'
+import { Table, Tooltip, Flex, TimePicker } from 'antd'
+import './sass/time-matches.module.scss'
 
 function TableTimeMatches(){
     const columns = [
@@ -16,22 +17,22 @@ function TableTimeMatches(){
         {
             title: <Tooltip title="Попытка №1">Попытка №1</Tooltip>,
             dataIndex: 'first_attempt',
-            key: 'first_attempt'
+            key: 'first_attempt',
         },
         {
             title: <Tooltip title="Попытка №2">Попытка №2</Tooltip>,
             dataIndex: 'second_attempt',
-            key: 'second_attempt'
+            key: 'second_attempt',
         },
         {
             title: <Tooltip title="Попытка №3">Попытка №3</Tooltip>,
             dataIndex: 'third_attempt',
-            key: 'third_attempt'
+            key: 'third_attempt',
         },
         {
             title: <Tooltip title="Лучшее время">Лучшее время</Tooltip>,
             dataIndex: 'best_attempt',
-            key: 'best_attempt'
+            key: 'best_attempt',
         },
         
     ]
@@ -40,12 +41,14 @@ function TableTimeMatches(){
         {
             place: '1',
             participants: 'Иванов Иван Иванович',
-            first_attempt: '2:34:12',
-            second_attempt: '2:45:34',
-            third_attempt: '2:47:35',
+            first_attempt: <TimePicker></TimePicker>,
+            second_attempt: <TimePicker></TimePicker>,
+            third_attempt: <TimePicker></TimePicker>,
             best_attempt: '2:49:35'
         }
     ]
+
+
 
     return (
         <Flex vertical gap="large">
