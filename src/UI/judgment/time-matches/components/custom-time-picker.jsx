4669e25c949001fa6afb2318d42b3a304177edb1
Ann-i <59306747+Ann-i@users.noreply.github.com>
2024-08-03 00:57:49 +0300

    feat: the component `CustomTimePicker` has been improved

--- src/UI/judgment/time-matches/components/custom-time-picker.jsx
+++ src/UI/judgment/time-matches/components/custom-time-picker.jsx
@@ -1,18 +1,47 @@
-import { TimePicker } from 'antd'
-import locale from 'antd/es/date-picker/locale/ru_RU'
+import { Button, TimePicker } from 'antd'
+import dayjs from 'dayjs'
+import { useState } from 'react'
+
+const format = 'mm:ss.SSS'
+const defaultTime = '00:00.000'
 
 export const CustomTimePicker = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOnOk = () => {
+    setIsOpen(false)
+  }
+
+  const handleOnInputClick = () => {
+    setIsOpen(true)
+  }
+
   return (
     <TimePicker
-      locale={{
-        ...locale,
-        lang: {
-          ...locale.lang,
-          now: 'Дискв.',
-          ok: 'Ок',
-        },
-      }}
-      format={{ format: 'mm:ss.SSS', type: 'mask' }}
+      onClick={handleOnInputClick}
+      open={isOpen}
+      variant="borderless"
+      needConfirm={false}
+      showNow={false}
+      defaultValue={dayjs(defaultTime, format)}
+      defaultOpenValue={dayjs(defaultTime, format)}
+      format={{ format, type: 'mask' }}
+      renderExtraFooter={() => (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            margin: '8px 0',
+          }}
+        >
+          <Button size="small" type="text">
+            Дискв.
+          </Button>
+          <Button size="small" type="primary" onClick={handleOnOk}>
+            Ок
+          </Button>
+        </div>
+      )}
     />
   )
 }
