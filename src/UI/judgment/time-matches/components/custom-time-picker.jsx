83666973cd84ec2100828f738576db734af13244
Ann-i <annadovzhik33@gmail.com>
2024-08-06 15:16:21 +0300

    feat: added new functionality for `CustomTimePicker`

--- src/UI/judgment/time-matches/components/custom-time-picker.jsx
+++ src/UI/judgment/time-matches/components/custom-time-picker.jsx
@@ -6,42 +6,71 @@ const format = "mm:ss.SSS";
 const defaultTime = "00:00.000";
 
 export const CustomTimePicker = () => {
+  const [value, setValue] = useState(dayjs(defaultTime, format));
+  const [isDisqualified, setIsDisqualified] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOnOk = () => {
+  const handleOk = () => {
     setIsOpen(false);
   };
 
-  const handleOnInputClick = () => {
+  const handleDisqualify = () => {
+    setIsDisqualified((disqualified) => !disqualified);
+  };
+
+  const handleInputClick = () => {
     setIsOpen(true);
   };
 
   return (
-    <TimePicker
-      onClick={handleOnInputClick}
-      open={isOpen}
-      variant="borderless"
-      needConfirm={false}
-      showNow={false}
-      defaultValue={dayjs(defaultTime, format)}
-      defaultOpenValue={dayjs(defaultTime, format)}
-      format={{ format, type: "mask" }}
-      renderExtraFooter={() => (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            margin: "8px 0",
-          }}
-        >
-          <Button size="small" type="text">
-            Дискв.
-          </Button>
-          <Button size="small" type="primary" onClick={handleOnOk}>
-            Ок
-          </Button>
-        </div>
+    <>
+      {!isDisqualified ? (
+        <TimePicker
+          value={value}
+          onChange={(e) => setValue(e)}
+          variant="borderless"
+          open={isOpen}
+          onClick={handleInputClick}
+          showNow={false}
+          needConfirm={false}
+          defaultValue={dayjs(defaultTime, format)}
+          defaultOpenValue={dayjs(defaultTime, format)}
+          format={{ format, type: "mask" }}
+          changeOnScroll
+          renderExtraFooter={() => (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "stretch",
+                margin: "8px 0",
+                gap: "8px",
+              }}
+            >
+              <Button
+                size="middle"
+                type="text"
+                style={{ width: "100%" }}
+                onClick={handleDisqualify}
+              >
+                Дискв.
+              </Button>
+              <Button
+                size="middle"
+                type="primary"
+                style={{ width: "100%" }}
+                onClick={handleOk}
+              >
+                Ок
+              </Button>
+            </div>
+          )}
+        />
+      ) : (
+        <span style={{ cursor: "pointer" }} onClick={() => handleDisqualify()}>
+          Дискв.
+        </span>
       )}
-    />
+    </>
   );
 };
