9ae444fdd3f4c04987f200aac28bb79cb4b21918
CultDog <kabakv09@gmail.com>
2024-08-11 19:04:27 +0300

    "Modified entrypoint.sh to remove server start and change file mode to executable"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-# Build the React app
 echo "Building the React app..."
 npm run build:prod
 
-# Optional: Start a server (if applicable)
-# echo "Starting the server..."
-# npm start
-
-# Keep the container running after the build
 echo "Build completed. Keeping the container alive..."
 tail -f /dev/null
