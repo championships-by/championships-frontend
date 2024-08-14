b35d4e4c0f857a1aa4188999daf643a55fb85e71
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-08-15 01:07:18 +0300

    refactor: the variable was taken out as a component

--- src/modules/judgment/groupStage/components/TableGroupStage.jsx
+++ src/modules/judgment/groupStage/components/TableGroupStage.jsx
@@ -2,40 +2,40 @@ import { useMatches } from "@hooks";
 import { Checkbox, Table } from "antd";
 import "./TableGroupStage.scss";
 
+const columns = [
+  {
+    title: "№",
+    key: "index",
+    dataIndex: "index",
+    render: (text, record, index) => <p>{index + 1}</p>,
+  },
+  {
+    title: "Участники",
+    key: "participant",
+    dataIndex: "participant",
+  },
+  {
+    title: "Очки",
+    key: "points",
+    dataIndex: "points",
+  },
+  {
+    title: "Счет",
+    key: "score",
+    dataIndex: "score",
+  },
+  {
+    title: "Участники",
+    key: "isPassed",
+    dataIndex: "isPassed",
+    render: (text, record) => (
+      <Checkbox defaultChecked={record.isPassed} disabled />
+    ),
+  },
+];
+
 export const TableGroupStage = () => {
   const { finalParticipants } = useMatches();
-
-  const columns = [
-    {
-      title: "№",
-      key: "index",
-      dataIndex: "index",
-      render: (text, record, index) => <p>{index + 1}</p>,
-    },
-    {
-      title: "Участники",
-      key: "participant",
-      dataIndex: "participant",
-    },
-    {
-      title: "Очки",
-      key: "points",
-      dataIndex: "points",
-    },
-    {
-      title: "Счет",
-      key: "score",
-      dataIndex: "score",
-    },
-    {
-      title: "Участники",
-      key: "isPassed",
-      dataIndex: "isPassed",
-      render: (text, record) => (
-        <Checkbox defaultChecked={record.isPassed} disabled />
-      ),
-    },
-  ];
 
   return !finalParticipants || finalParticipants.length === 0 ? (
     <div className="no-data">
