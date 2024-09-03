ccc3cbf9388d7966fdadc5a682da17f545f4927a
motfisi <108813531+motfisi@users.noreply.github.com>
2024-09-04 00:39:23 +0300

    fix

--- src/components/userSupport/AboutProgram.jsx
+++ src/components/userSupport/AboutProgram.jsx
@@ -4,6 +4,7 @@ import bntuLogo from "@assets/img/bntu-logo.png";
 import fitrLogo from "@assets/img/fitr-logo.png";
 import schoolLogo from "@assets/img/school-logo.png";
 import laptop from "@assets/img/laptop.png";
+import { bntuSite, zubronokSite, fitrSite, gymnSite } from "@constants";
 
 import "./sass/user-support.scss";
 
@@ -11,12 +12,16 @@ function AboutProgram() {
   return (
     <div className="user-support">
       <Typography.Title level={2}>О портале</Typography.Title>
-      <Typography.Title level={3}>
-        Республиканская единая система управления соревнованиями
-      </Typography.Title>
-      <Row>
-        <Col span={13}>
-          <Typography.Title level={4}>«Наши преимущества»</Typography.Title>
+      <Typography.Text className="user-support__about__text">
+        Республиканская единая система управления соревнованиями - это система
+        управления мероприятиями и соревнованиями. Все процессы
+        автоматизированы, что позволяет экономить ресурсы и время на организацию
+        проведения соревнований разного уровня от городского до
+        республиканского.
+      </Typography.Text>
+      <Row align="middle">
+        <Col span={13} className="user-support__about__text1">
+          <Typography.Title level={3}>Преимущества</Typography.Title>
           <Typography.Text strong className="user-support__about__text">
             1. Удобство:{" "}
           </Typography.Text>
@@ -52,21 +57,27 @@ function AboutProgram() {
       </Row>
       <Row justify="center" align="middle">
         <Col>
-          <Typography.Title level={4}>Разработчики</Typography.Title>
+          <Typography.Title level={3}>Разработчики</Typography.Title>
         </Col>
       </Row>
       <Row>
         <Col span={8} className="user-support__about__logo-container">
-          <a href="">
+          <a href={zubronokSite}>
             <img src={logo} className="user-support__about__logo" />
           </a>
         </Col>
         <Col span={8} className="user-support__about__logo-container">
-          <img src={fitrLogo} className="user-support__about__fitrLogo" />
-          <img src={bntuLogo} className="user-support__about__bntuLogo" />
+          <a href={fitrSite}>
+            <img src={fitrLogo} className="user-support__about__fitrLogo" />
+          </a>
+          <a href={bntuSite}>
+            <img src={bntuLogo} className="user-support__about__bntuLogo" />
+          </a>
         </Col>
         <Col span={8} className="user-support__about__logo-container">
-          <img src={schoolLogo} className="user-support__about__logo" />
+          <a href={gymnSite}>
+            <img src={schoolLogo} className="user-support__about__logo" />
+          </a>
         </Col>
       </Row>
       <Row>
@@ -91,7 +102,7 @@ function AboutProgram() {
             Государственное учреждение образования{" "}
           </Typography.Text>
           <Typography.Text strong className="user-support__about__institution">
-            «Гимназия 61 г. Минска»
+            «Гимназия №61 г. Минска»
           </Typography.Text>
         </Col>
       </Row>
