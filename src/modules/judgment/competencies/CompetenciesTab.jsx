ed1717199a846a29739f364c3742acc3e907ac69
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-09-04 08:45:37 +0300

    refactor: the way tabs are displayed has been changed

--- src/modules/judgment/competencies/CompetenciesTab.jsx
+++ src/modules/judgment/competencies/CompetenciesTab.jsx
@@ -1,6 +1,7 @@
 import { tabsButtonEventEmitter, TabsButtonEvents } from "@constants";
 import { useTabs } from "@hooks/useTabs";
 import { Button, Tabs } from "antd";
+import { CompetenciesResults, CompetenciesTable } from "./components";
 
 function CompetenciesTab() {
   const { tabs } = useTabs();
@@ -12,7 +13,16 @@ function CompetenciesTab() {
 
   return (
     <Tabs
-      items={tabs}
+      items={[
+        {
+          ...tabs[0],
+          children: <CompetenciesTable />,
+        },
+        {
+          ...tabs[1],
+          children: <CompetenciesResults />,
+        },
+      ]}
       tabBarExtraContent={{
         right: (
           <Button onClick={onClick} type="primary">
--- src/modules/judgment/competencies/constants/index.js
+++ src/modules/judgment/competencies/constants/index.js
@@ -1,5 +1,3 @@
-import { CompetenciesResults, CompetenciesTable } from "../components";
-
 export const CompetenciesTabs = {
   TABLE: "table",
   RESULTS: "results",
@@ -9,12 +7,10 @@ export const competenciesTabs = {
   [CompetenciesTabs.TABLE]: {
     key: "1",
     label: "Таблица",
-    children: <CompetenciesTable />,
   },
   [CompetenciesTabs.RESULTS]: {
     key: "2",
     label: "Итоги",
-    children: <CompetenciesResults />,
     disabled: true,
   },
 };
