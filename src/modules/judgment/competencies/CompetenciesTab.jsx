dd0fda91e7afb551c7d886db00ef62867aad4b50
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-09-04 15:03:44 +0300

    refactor: improved the code of the competence table

--- src/modules/judgment/competencies/CompetenciesTab.jsx
+++ src/modules/judgment/competencies/CompetenciesTab.jsx
@@ -1,32 +1,133 @@
-import { tabsButtonEventEmitter, TabsButtonEvents } from "@constants";
 import { useTabs } from "@hooks/useTabs";
 import { Button, Tabs } from "antd";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { competenciesApi } from "../../../api";
+import { RESPONSE_STATUS } from "../../../constants";
+import {
+  generateCompetenciesDataSource,
+  transformCriteriaData,
+  transformCriteriaResultsData,
+  transformStageStatus,
+} from "../../../utils";
 import { CompetenciesResults, CompetenciesTable } from "./components";
 
 function CompetenciesTab() {
   const { tabs } = useTabs();
+  const { eventId, nominationId } = useParams();
 
-  const onClick = (e) => {
-    e.preventDefault();
-    tabsButtonEventEmitter.emit(TabsButtonEvents.ON_CLICK);
-  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [stageStatus, setStageStatus] = useState({});
+  const [criteria, setCriteria] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [isErrorOccurred, setIsErrorOccurred] = useState(false);
+  const [isStageFinished, setIsStageFinished] = useState(false);
+
+  const handleCompleteStage = useCallback(() => {}, []);
+
+  const handleDownload = useCallback(() => {}, []);
+
+  const handleChange = useCallback(
+    (value, index, columnId) => {
+      const newDataSource = [...dataSource];
+      newDataSource[index][`criteria${columnId}`].score = value;
+      newDataSource[index].totalScore = Object.keys(newDataSource[index])
+        .filter((key) => key.startsWith("criteria"))
+        .reduce((acc, key) => acc + newDataSource[index][key].score, 0);
+      setDataSource(newDataSource);
+    },
+    [dataSource]
+  );
+
+  useEffect(() => {
+    if (!isDataLoaded) {
+      setIsLoading(true);
+      Promise.all([
+        competenciesApi.getNominationEventInfo({ eventId, nominationId }),
+        competenciesApi.getCriteria(eventId, nominationId),
+        competenciesApi.getCriteriaResults(eventId, nominationId),
+      ])
+        .then(
+          ([
+            stageStatusResponse,
+            criteriaResponse,
+            criteriaResultsResponse,
+          ]) => {
+            if (stageStatusResponse.status === RESPONSE_STATUS.STATUS_OK) {
+              const transformedStageStatus = transformStageStatus(
+                stageStatusResponse.data
+              );
+              setStageStatus(transformedStageStatus);
+
+              if (stageStatus.tournamentFinished) {
+                setIsStageFinished(true);
+              }
+            }
+
+            if (criteriaResponse.status === RESPONSE_STATUS.STATUS_OK) {
+              const transformedCriteria = transformCriteriaData(
+                criteriaResponse.data
+              );
+              setCriteria(transformedCriteria);
+            }
+
+            if (criteriaResultsResponse.status === RESPONSE_STATUS.STATUS_OK) {
+              const transformedCriteriaResults = transformCriteriaResultsData(
+                criteriaResultsResponse.data
+              );
+              const generatedDataSource = generateCompetenciesDataSource(
+                transformedCriteriaResults
+              );
+              setDataSource(generatedDataSource);
+            }
+          }
+        )
+        .catch(() => setIsErrorOccurred(true))
+        .finally(() => {
+          setIsLoading(false);
+          setIsDataLoaded(true);
+        });
+    }
+  }, [eventId, isDataLoaded, nominationId, stageStatus]);
 
   return (
     <Tabs
       items={[
         {
           ...tabs[0],
-          children: <CompetenciesTable />,
+          children: (
+            <CompetenciesTable
+              criteria={criteria}
+              dataSource={dataSource}
+              isLoading={isLoading}
+              hasError={isErrorOccurred}
+              onChange={handleChange}
+              editable={
+                stageStatus.registrationFinished &&
+                stageStatus.tournamentStarted &&
+                !stageStatus.tournamentFinished
+              }
+            />
+          ),
         },
         {
           ...tabs[1],
+          disabled: !(
+            stageStatus.registrationFinished &&
+            stageStatus.tournamentStarted &&
+            stageStatus.tournamentFinished
+          ),
           children: <CompetenciesResults />,
         },
       ]}
       tabBarExtraContent={{
         right: (
-          <Button onClick={onClick} type="primary">
-            Завершить этап
+          <Button
+            onClick={isStageFinished ? handleDownload : handleCompleteStage}
+            type="primary"
+          >
+            {isStageFinished ? "Итоговый протокол" : "Завершить этап"}
           </Button>
         ),
       }}
--- src/modules/judgment/competencies/components/CompetenciesTable.jsx
+++ src/modules/judgment/competencies/components/CompetenciesTable.jsx
@@ -1,25 +1,16 @@
 import { LoadingOutlined } from "@ant-design/icons";
-import { competenciesApi } from "@api";
-import { tabsButtonEventEmitter, TabsButtonEvents } from "@constants";
-import {
-  generateCompetenciesDataSource,
-  generateCriteriaColumns,
-  transformCriteriaData,
-  transformCriteriaResultsData,
-} from "@utils";
+import { generateCriteriaColumns } from "@utils";
 import { InputNumber, Spin, Table } from "antd";
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useMemo } from "react";
 
-export const CompetenciesTable = () => {
-  const [error, setError] = useState();
-  const [criteria, setCriteria] = useState([]);
-  const [dataSource, setDataSource] = useState([]);
-  const [changes, setChanges] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const { eventId, nominationId } = useParams();
-
+export const CompetenciesTable = ({
+  criteria,
+  dataSource,
+  isLoading,
+  hasError,
+  editable,
+  onChange,
+}) => {
   const columns = useMemo(
     () => [
       {
@@ -35,47 +26,14 @@ export const CompetenciesTable = () => {
         const currentCriteria = record[`criteria${columnId}`];
         return (
           <InputNumber
+            disabled={!editable}
             placeholder={currentCriteria.maxScore}
             defaultValue={currentCriteria.score}
             max={currentCriteria.maxScore}
             min={0}
-            onChange={(value) => {
-              const newDataSource = [...dataSource];
-              newDataSource[index][`criteria${columnId}`].score = value;
-              newDataSource[index].totalScore = Object.keys(
-                newDataSource[index]
-              )
-                .filter((key) => key.startsWith("criteria"))
-                .reduce((acc, key) => acc + newDataSource[index][key].score, 0);
-              setDataSource(newDataSource);
-
-              const newChanges = [...changes];
-              const existingChangeIndex = newChanges.findIndex(
-                (change) =>
-                  change.criteria_id === currentCriteria.id &&
-                  change.team_id === record.team.id
-              );
-
-              if (existingChangeIndex !== -1) {
-                if (value === currentCriteria.initialScore) {
-                  newChanges.splice(existingChangeIndex, 1);
-                } else {
-                  newChanges[existingChangeIndex].score = value;
-                }
-              } else if (value !== currentCriteria.initialScore) {
-                newChanges.push({
-                  criteriaId: currentCriteria.id,
-                  teamId: record.team.id,
-                  score: value,
-                });
-              }
-
-              setChanges(
-                newChanges.filter(
-                  (change) => change.score !== currentCriteria.initialScore
-                )
-              );
-            }}
+            onChange={(value) =>
+              onChange(value, index, columnId, currentCriteria)
+            }
           />
         );
       }),
@@ -85,55 +43,12 @@ export const CompetenciesTable = () => {
         key: "totalScore",
       },
     ],
-    [criteria, dataSource, changes]
+    [criteria, editable, onChange]
   );
-
-  const handleCompleteStage = useCallback(() => {
-    if (changes.length <= 0) return;
-
-    changes.forEach(({ criteriaId, teamId, score }) => {
-      competenciesApi.setCriteriaResult({
-        eventId,
-        nominationId,
-        criteriaId,
-        teamId,
-        score,
-      });
-    });
-  }, [changes, eventId, nominationId]);
-
-  useEffect(() => {
-    setIsLoading(true);
-    Promise.all([
-      competenciesApi.getCriteria(eventId, nominationId),
-      competenciesApi.getCriteriaResults(eventId, nominationId),
-    ])
-      .then(([criteriaResponse, criteriaResultsResponse]) => {
-        const criteriaData = transformCriteriaData(criteriaResponse.data);
-        setCriteria(criteriaData);
-
-        const criteriaResultsData = transformCriteriaResultsData(
-          criteriaResultsResponse.data
-        );
-        const generatedDataSource =
-          generateCompetenciesDataSource(criteriaResultsData);
-        setDataSource(generatedDataSource);
-
-        setIsLoading(false);
-      })
-      .catch((reason) => {
-        setError(reason);
-        setIsLoading(false);
-      });
-  }, [eventId, nominationId]);
-
-  useEffect(() => {
-    tabsButtonEventEmitter.on(TabsButtonEvents.ON_CLICK, handleCompleteStage);
-  }, [handleCompleteStage]);
 
   return isLoading ? (
     <Spin indicator={<LoadingOutlined className="icon" spin />} />
-  ) : error ? (
+  ) : hasError ? (
     <h1>Произошла ошибка</h1>
   ) : (
     <Table columns={columns} dataSource={dataSource} />
