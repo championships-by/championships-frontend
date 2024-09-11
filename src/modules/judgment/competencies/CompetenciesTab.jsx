1b2ff8fec3d12e443ba5e0002f99f153ad625b54
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-09-11 23:56:40 +0300

    feat: fixed incorrect stage completion method

--- src/modules/judgment/competencies/CompetenciesTab.jsx
+++ src/modules/judgment/competencies/CompetenciesTab.jsx
@@ -48,7 +48,7 @@ function CompetenciesTab() {
         });
       });
 
-      await competenciesApi.finishTimeStage({
+      await competenciesApi.finishCriteriaStage({
         event_id: eventId,
         nomination_id: nominationId,
       });
