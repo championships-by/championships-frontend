c3efa6389ad2d7277e3d7691292cd08a0f7a6819
Alexander Skvorchevskiy <sashaskvorhevskiy@gmail.com>
2024-09-23 20:47:03 +0300

    fix: conflicts has been resolved

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,7 +1,6 @@
 export const ERRORS = {
   "email taken": "Введённый Email уже занят",
   "logged out": "Для доступа к системе необходима авторизация",
-
   getError: function (errorKey) {
     return this[errorKey] || "Неизвестная ошибка";
   },
