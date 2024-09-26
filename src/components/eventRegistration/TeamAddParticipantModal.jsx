eae3af927a2d2c1eca899c78e16387dc78900dc9
motfisi <108813531+motfisi@users.noreply.github.com>
2024-09-27 00:19:28 +0300

    reset new modal

--- src/components/eventRegistration/TeamAddParticipantModal.jsx
+++ src/components/eventRegistration/TeamAddParticipantModal.jsx
@@ -27,6 +27,7 @@ function TeamAddParticipantModal({ isOpen, onOk, onCancel, teamID }) {
 
   useEffect(() => {
     if (isOpen) {
+      form.resetFields();
       eventApi
         .getEvent(eventID)
         .then((data) => {
@@ -49,7 +50,7 @@ function TeamAddParticipantModal({ isOpen, onOk, onCancel, teamID }) {
           )
         );
     }
-  }, [isOpen, eventID]);
+  }, [isOpen, eventID, teamID]);
 
   const onNominationChange = (nominationID) => {
     setNominationId(nominationID);
