17b034466d662bd485b19caa4bf0acfa2644d324
motfisi <108813531+motfisi@users.noreply.github.com>
2024-10-16 20:07:44 +0300

    dark-theme-elements

--- src/components/themeToggle/themeToggle.jsx
+++ src/components/themeToggle/themeToggle.jsx
@@ -1,14 +1,32 @@
+import React, { useState, useEffect } from "react";
 import { FloatButton } from "antd";
-import { MoonOutlined, SunFilled } from "@ant-design/icons";
+import { MoonOutlined, SunOutlined } from "@ant-design/icons";
 
 function ThemeToggle() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.className = theme === "light" ? "" : "dark-theme";
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
   return (
     <>
       <FloatButton
-        style={{
-          insetInlineEnd: 94,
-        }}
-        icon={<MoonOutlined />}
+        style={{ insetInlineEnd: 94 }}
+        icon={theme === "light" ? <MoonOutlined /> : <SunOutlined />}
+        onClick={toggleTheme}
       />
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./sass/index.scss";
 import App from "./App.jsx";
+import "@src/sass/lightTheme/index.scss";
+import "@src/sass/darkTheme/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
