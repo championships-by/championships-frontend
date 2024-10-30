3aebac2db99ed1767e5c3306b199428f44c67965
motfisi <108813531+motfisi@users.noreply.github.com>
2024-10-30 12:32:01 +0300

    add isDesktop

--- src/hooks/useDevice.js
+++ src/hooks/useDevice.js
@@ -3,5 +3,6 @@ import { useMediaQuery } from "react-responsive";
 export function useDevice() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
-  return { isMobile, isTablet };
+  const isDesktop = useMediaQuery({ minWidth: 991 });
+  return { isMobile, isTablet, isDesktop };
 }
