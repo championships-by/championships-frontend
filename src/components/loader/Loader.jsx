4dc79b77ddd0adf5c563289b2fbd6ad2cdb869a2
motfisi <108813531+motfisi@users.noreply.github.com>
2024-10-31 15:38:23 +0300

    loader fix

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import loader from "@assets/img/loader.gif";
 import loaderDark from "@assets/img/loaderDark.gif";
 
 import "./sass/loader.scss";
 
 function Loader({ show, ...props }) {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme) {
-      setTheme(storedTheme);
-    }
-  }, []);
-
   return (
     <div className={`loader ${!show && "hide"}`} {...props}>
-      <img
-        src={theme === "light" ? loader : loaderDark}
-        className="loader-gif"
-      />
+      <div className="loader-gif" />
     </div>
   );
 }
