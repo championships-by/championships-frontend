b1021048e3848dbbf5419d96a8da3ec07f016307
motfisi <108813531+motfisi@users.noreply.github.com>
2025-01-23 22:05:51 +0300

    remove space

--- src/api/competencies.js
+++ src/api/competencies.js
@@ -85,7 +85,6 @@ export const competenciesApi = {
       params,
       responseType: "blob",
     }),
-
   editNumberRaceRounds: (params) =>
     instance.patch(
       `${API_PATH}/nomination_event/edit_number_of_race_rounds`,
