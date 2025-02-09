2681572d2085d2276ac5ef6fa4a396220b49d668
motfisi <matthew.ermakovich@gmail.com>
2025-02-09 14:52:58 +0300

    fix: comments

--- src/modules/judgment/events/EventDescription.jsx
+++ src/modules/judgment/events/EventDescription.jsx
@@ -1,11 +1,9 @@
-import { useLayoutEffect } from "react";
 import { Typography, Flex } from "antd";
 import { validateDescription } from "@utils";
 import FormItem from "antd/es/form/FormItem";
-import ReactQuill from "react-quill";
 import { useTranslation } from "react-i18next";
+import TextEditor from "@modules/textEditor/TextEditor";
 
-import "react-quill/dist/quill.snow.css";
 import "./sass/events.scss";
 
 function EventDescription({ name, value, form, onChange: onChangeBase }) {
@@ -18,33 +16,6 @@ function EventDescription({ name, value, form, onChange: onChangeBase }) {
 
     form.setFieldsValue({ [name]: newValue });
   };
-
-  useLayoutEffect(() => {
-    const styleSheet = document.styleSheets[0];
-
-    if (styleSheet) {
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip.ql-editing a.ql-action::after { content: "${t(
-          "COMMON.SAVE"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip a.ql-action::after { content: "${t(
-          "COMMON.EDIT"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip a.ql-remove::before { content: "${t(
-          "COMMON.DELETE"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-    }
-  }, [t]);
 
   return (
     <FormItem
@@ -63,19 +34,7 @@ function EventDescription({ name, value, form, onChange: onChangeBase }) {
     >
       <Flex vertical>
         <Typography.Text>{t("EVENTS.EVENT_DESCRIPTION")}</Typography.Text>
-        <ReactQuill
-          value={value}
-          onChange={onChange}
-          placeholder={t("EVENTS.ENTER_EVENT_DESCRIPTION")}
-          className="events__event-description__quill"
-          modules={{
-            toolbar: [
-              ["bold", "italic", "underline"],
-              [{ list: "ordered" }, { list: "bullet" }],
-              ["link"],
-            ],
-          }}
-        />
+        <TextEditor value={value} onChange={onChange} />
       </Flex>
     </FormItem>
   );
--- src/modules/judgment/events/EventRequirements.jsx
+++ src/modules/judgment/events/EventRequirements.jsx
@@ -1,9 +1,8 @@
-import { useLayoutEffect } from "react";
 import { Typography, Flex } from "antd";
 import { validateDescription } from "@utils";
 import FormItem from "antd/es/form/FormItem";
 import { useTranslation } from "react-i18next";
-import ReactQuill from "react-quill";
+import TextEditor from "@modules/textEditor/TextEditor";
 
 import "react-quill/dist/quill.snow.css";
 import "./sass/events.scss";
@@ -18,33 +17,6 @@ function EventRequirements({ name, value, form, onChange: onChangeBase }) {
 
     form.setFieldsValue({ [name]: newValue });
   };
-
-  useLayoutEffect(() => {
-    const styleSheet = document.styleSheets[0];
-
-    if (styleSheet) {
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip.ql-editing a.ql-action::after { content: "${t(
-          "COMMON.SAVE"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip a.ql-action::after { content: "${t(
-          "COMMON.EDIT"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-
-      styleSheet.insertRule(
-        `.events__event-description__quill .ql-snow .ql-tooltip a.ql-remove::before { content: "${t(
-          "COMMON.DELETE"
-        )}"; }`,
-        styleSheet.cssRules.length
-      );
-    }
-  }, [t]);
 
   return (
     <FormItem
@@ -65,19 +37,7 @@ function EventRequirements({ name, value, form, onChange: onChangeBase }) {
         <Typography.Text>
           {t("COMMON.WHAT_NEED_TO_PARTICIPATE")}
         </Typography.Text>
-        <ReactQuill
-          value={value}
-          onChange={onChange}
-          placeholder={t("COMMON.ENTER_REQUIREMENTS")}
-          className="events__event-description__quill"
-          modules={{
-            toolbar: [
-              ["bold", "italic", "underline"],
-              [{ list: "ordered" }, { list: "bullet" }],
-              ["link"],
-            ],
-          }}
-        />
+        <TextEditor value={value} onChange={onChange} />
       </Flex>
     </FormItem>
   );
