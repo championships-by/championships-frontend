fd7e9b04205731ba5ba4a03b52b68d95a2cdd962
mivanoffka <sharariki@gmail.com>
2025-02-12 11:59:56 +0300

    style: update cookies message

--- src/components/cookies/CookiesNotification.jsx
+++ src/components/cookies/CookiesNotification.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
-import { notification, Space, Button } from "antd";
+import { notification, Space, Button, Typography, Flex } from "antd";
 import { useTranslation } from "react-i18next";
+import { splitByCookies } from "@utils";
 
 function CookiesNotification() {
   const [api, contextHolder] = notification.useNotification();
   const { t } = useTranslation();
+  const cookiesMessageParts = splitByCookies(t("COMMON.COOKIES_MESSAGE"));
 
   useEffect(() => {
     const hasAcceptedCookies = localStorage.getItem("cookiesAccepted");
@@ -17,18 +19,25 @@ function CookiesNotification() {
     };
 
     const btn = (
-      <Space>
-        <Button type="primary" size="small" onClick={handleAccept}>
-          {t("COMMON.OK")}
-        </Button>
-      </Space>
+      <Button type="primary" size="small" onClick={handleAccept}>
+        {t("COMMON.AGREE")}
+      </Button>
     );
 
     api.info({
-      message: t("COMMON.ATTENTION"),
-      description: t("COMMON.COOKIES_MESSAGE"),
+      message: (
+        <Typography.Text strong>{t("COMMON.ATTENTION")}</Typography.Text>
+      ),
+      description: (
+        <Typography>
+          <Typography.Text>{cookiesMessageParts[0]}</Typography.Text>
+          <Typography.Link>{cookiesMessageParts[1]}</Typography.Link>
+          <Typography.Text>{cookiesMessageParts[2]}</Typography.Text>
+        </Typography>
+      ),
       btn,
       placement: "bottomRight",
+      duration: 0,
       closeIcon: null,
     });
   }, [api, t]);
