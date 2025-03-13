0bc0db8b1340c1bbcf2413970e80a7f6f55ea855
KINGOFMUSIC <GoldonGame.06@gmail.com>
2025-03-13 13:48:24 +0500

    fix: fixed play off when teams was null in json

--- src/utils/index.js
+++ src/utils/index.js
@@ -597,6 +597,10 @@ export const getTreeData = (leveledMatches, handleEditScore) => {
   let overallIndex = 0;
   leveledMatches.forEach((level, levelIndex) => {
     level.forEach((match, index) => {
+      if (!match.team1 && !match.team2) {
+        console.warn(`Пропускаем матч ${match.id}, так как у него нет команд.`);
+        return;
+      }
       nodes.push({
         id: match.id.toString(),
         data: {
